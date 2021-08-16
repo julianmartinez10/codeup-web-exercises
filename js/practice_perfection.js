b872c7e3505c8028d78097d195b33032fc29879c
Julian Martinez <julianmartinez1830@gmail.com>
2021-08-16 12:31:55 -0500

    Commented out previous practice code

--- js/practice_perfection.js
+++ js/practice_perfection.js
@@ -1,118 +1,121 @@
 "use strict";
 
 
-// Make a function called returnTwo() that returns the number 2 when called
-function returnTwo(){
-    return 2;
-}
-// Test this function with console.log(returnTwo())
-console.log(returnTwo());
-// Make a function called sayHowdy() which console.logs the string “Howdy!”
-function sayHowdy(){
-    console.log("Howdy!");
-}
-// Test this function by directly calling sayHowdy()
-// Remember this function does not need a defined return value
-sayHowdy();
-// Make a function called returnName() that returns the string of your name
-function returnName(){
-    return "Julian";
-}
-// Test this function with console.log(returnName())
-console.log(returnName());
-// Make a function called addThree() which takes in a number input and returns the number plus 3.
-function addThree(num){
-    return num + 3;
-}
-// Test this function with console.log(addThree(5))
-console.log(addThree(5));
-// Make a function called sayString() which returns the string input passed in.
-function sayString(someString){
-    return someString;
-}
-// Test this function with console.log(sayString('codeup'))
-console.log(sayString('codeup'));
+// // Make a function called returnTwo() that returns the number 2 when called
+// function returnTwo(){
+//     return 2;
+// }
+// // Test this function with console.log(returnTwo())
+// console.log(returnTwo());
+// // Make a function called sayHowdy() which console.logs the string “Howdy!”
+// function sayHowdy(){
+//     console.log("Howdy!");
+// }
+// // Test this function by directly calling sayHowdy()
+// // Remember this function does not need a defined return value
+// sayHowdy();
+// // Make a function called returnName() that returns the string of your name
+// function returnName(){
+//     return "Julian";
+// }
+// // Test this function with console.log(returnName())
+// console.log(returnName());
+// // Make a function called addThree() which takes in a number input and returns the number plus 3.
+// function addThree(num){
+//     return num + 3;
+// }
+// // Test this function with console.log(addThree(5))
+// console.log(addThree(5));
+// // Make a function called sayString() which returns the string input passed in.
+// function sayString(someString){
+//     return someString;
+// }
+// // Test this function with console.log(sayString('codeup'))
+// console.log(sayString('codeup'));
+//
+// //     Challenge Function Drills
+// //     1,000 hours
+// // Write a function called identity(input) that takes in an argument called input and returns that input.
+// function identity(input){
+//     console.log(input);
+//     return input;
+// }
+// // Write a function called getRandomNumber(min, max) that returns a random number between min
+// // and max values sent to that function call.
+// function getRandomNumber(min, max){
+//     var randomNum = Math.random() * (max - min) + min;
+//     console.log(randomNum)
+//     return Math.floor(randomNum);
+// }
+// // Write a function called first(input) that returns the first character in the provided string.
+// function first(input){
+//     return input.charAt(0);
+// }
+// // Write a function called last(input) that returns the last character of a string
+// function last(input){
+//     return input.charAt(input.length - 1);
+// }
+// // Write a function called rest(input) that returns everything but the first character of a string.
+// function rest(input){
+//     return input.substring(1, input.length);
+// }
+// // Write a function called reverse(input) that takes a string and returns it reversed.
+// function reverse(input){
+//
+//     return " ";
+// }
+// // Write a function called isNumeric(input) that takes an input and returns a boolean if the
+// // input is numeric.
+// function isNumeric(input){
+//     return Number.isInteger(input);
+//     // return Number.isInteger(Math.floor(input));
+// }
+// // Write a function called count(input) that takes in a string and returns the number of
+// // characters.
+// function count(input){
+//     var str = input.toString();
+//     return str.length;
+// }
+// // Write a function called add(a, b) that returns the sum of a and b
+// function add(a, b){
+//     return a + b;
+// }
+// // Write a function called subtract(a, b) that return the difference between the two inputs.
+// function subtract(a, b){
+//     return a-b;
+// }
+// // Write multiply(a, b) function that returns the product
+// function multiply(a, b){
+//     return a*b;
+// }
+// // Write a divide(numerator, denominator) function that returns a divided by b
+// function divide(a, b){
+//     return a/b;
+// }
+// // Write a remainder(number, divisor) function that returns the remainder left over when
+// // dividing number by the divisor
+// function remainder(number, divisor){
+//     return number % divisor;
+// }
+// // Write the function square(a) that takes in a number and returns the number multiplied by itself.
+// function square(a){
+//     return Math.pow(a, 2);
+// }
+// // Write a function called sumOfSquares(a, b) that uses only your add() function and your square
+// // function and not + or * operators
+// function sumOfSquares(a, b){
+//     var sqA = square(a);
+//     console.log(sqA);
+//     var sqB = square(b);
+//     console.log(sqB);
+//     return add(sqA, sqB);
+// }
+// // Write a function called doMath(operator, a, b) that takes 3 parameters. The first parameter
+// // is the name of the math function you want to apply. a and b are the two numbers to run that
+// // function on.
+// function doMath(operator, a, b){
+//     return ;
+// }
 
-//     Challenge Function Drills
-//     1,000 hours
-// Write a function called identity(input) that takes in an argument called input and returns that input.
-function identity(input){
-    console.log(input);
-    return input;
-}
-// Write a function called getRandomNumber(min, max) that returns a random number between min
-// and max values sent to that function call.
-function getRandomNumber(min, max){
-    var randomNum = Math.random() * (max - min) + min;
-    console.log(randomNum)
-    return Math.floor(randomNum);
-}
-// Write a function called first(input) that returns the first character in the provided string.
-function first(input){
-    return input.charAt(0);
-}
-// Write a function called last(input) that returns the last character of a string
-function last(input){
-    return input.charAt(input.length - 1);
-}
-// Write a function called rest(input) that returns everything but the first character of a string.
-function rest(input){
-    return input.substring(1, input.length);
-}
-// Write a function called reverse(input) that takes a string and returns it reversed.
-function reverse(input){
+//  ##########################CONDITIONAL FOLLOW ALONG##################################
 
-    return " ";
-}
-// Write a function called isNumeric(input) that takes an input and returns a boolean if the
-// input is numeric.
-function isNumeric(input){
-    return Number.isInteger(input);
-    // return Number.isInteger(Math.floor(input));
-}
-// Write a function called count(input) that takes in a string and returns the number of
-// characters.
-function count(input){
-    var str = input.toString();
-    return str.length;
-}
-// Write a function called add(a, b) that returns the sum of a and b
-function add(a, b){
-    return a + b;
-}
-// Write a function called subtract(a, b) that return the difference between the two inputs.
-function subtract(a, b){
-    return a-b;
-}
-// Write multiply(a, b) function that returns the product
-function multiply(a, b){
-    return a*b;
-}
-// Write a divide(numerator, denominator) function that returns a divided by b
-function divide(a, b){
-    return a/b;
-}
-// Write a remainder(number, divisor) function that returns the remainder left over when
-// dividing number by the divisor
-function remainder(number, divisor){
-    return number % divisor;
-}
-// Write the function square(a) that takes in a number and returns the number multiplied by itself.
-function square(a){
-    return Math.pow(a, 2);
-}
-// Write a function called sumOfSquares(a, b) that uses only your add() function and your square
-// function and not + or * operators
-function sumOfSquares(a, b){
-    var sqA = square(a);
-    console.log(sqA);
-    var sqB = square(b);
-    console.log(sqB);
-    return add(sqA, sqB);
-}
-// Write a function called doMath(operator, a, b) that takes 3 parameters. The first parameter
-// is the name of the math function you want to apply. a and b are the two numbers to run that
-// function on.
-function doMath(operator, a, b){
-    return ;
-}
