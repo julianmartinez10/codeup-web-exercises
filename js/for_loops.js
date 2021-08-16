096748f7d1877d2328f52ddd2d6481bb1d80297f
Julian Martinez <julianmartinez1830@gmail.com>
2021-08-16 15:22:37 -0500

    feat: added h1 tag to html, completed question2

---
+++ js/for_loops.js
@@ -0,0 +1,12 @@
+function showMultiplicationTable(num){
+    console.log(`${num} X 1 = ${(num*1)}`);
+    console.log(`${num} X 2 = ${(num*2)}`);
+    console.log(`${num} X 3 = ${(num*3)}`);
+    console.log(`${num} X 4 = ${(num*4)}`);
+    console.log(`${num} X 5 = ${(num*5)}`);
+    console.log(`${num} X 6 = ${(num*6)}`);
+    console.log(`${num} X 7 = ${(num*7)}`);
+    console.log(`${num} X 8 = ${(num*8)}`);
+    console.log(`${num} X 9 = ${(num*9)}`);
+    console.log(`${num} X 10 = ${(num*10)}`);
+}
