e2ea109e7913df01b6dfb42633256e06ab47e974
Julian Martinez <julianmartinez1830@gmail.com>
2021-08-17 10:16:17 -0500

    feat: completed questionbc2

--- js/break_and_continue.js
+++ js/break_and_continue.js
@@ -1,10 +1,23 @@
 "use strict";
 
-for(var i=0; i < 50; i++){
-    var pickOdd = prompt("Select an odd number between 1 and 50");
-    if(Number(pickOdd) % 2 !== 0){
-        console.log("This is an odd number")
+// var userInput = prompt("Select an odd number between 1 and 50");
+// var numPick = Number(userInput);
+
+for(var i=1; i <= 50; i++){
+    //  TRYING VARIABLE INSIDE AND OUTSIDE OF LOOP
+    var userInput = prompt("Select an odd number between 1 and 50");
+    var numPick = Number(userInput);
+    if(numPick % 2 === 0){
+        // alert("That's an even number. Please refresh and try again.")
+        console.log(`iteration #${i}`);
         continue;
+    }else{
+        console.log(`Thank you for selecting an odd number. ${numPick} is an odd number!`);
+        break;
     }
-    console.log("Here's an iteration?")
+    console.log("This is iteration #" + i);
 }
+console.log("You've reached the end of the loop");
+
+// Prompt the user for an odd number between 1 and 50. Use a loop and a break statement to continue prompting the user if they enter invalid input.
+// Use a loop and the continue statement to output all the odd numbers between 1 and 50, except for the number the user entered.
