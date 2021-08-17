1f104791eef3872fa3346fd1f4bc5b42d44b9fda
Julian Martinez <julianmartinez1830@gmail.com>
2021-08-16 20:52:23 -0500

    feat: started working on question3bc

--- js/break_and_continue.js
+++ js/break_and_continue.js
@@ -1,2 +1,11 @@
-var pickOdd = prompt("Select an odd number between 1 and 50");
+"use strict";
 
+for(var i=0; i < 50; i++){
+    var pickOdd = prompt("Select an odd number between 1 and 50");
+    if(Number(pickOdd) % 2 === 0){
+        continue;
+    }else{
+        console.log("Still working on this.");
+        break;
+    }
+}
