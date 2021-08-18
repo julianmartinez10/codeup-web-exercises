22f0451973d810d94b36fba45debd5d27b3cf409
Julian Martinez <julianmartinez1830@gmail.com>
2021-08-17 20:57:27 -0500

    feat: created a do while loop, not completed

--- js/do_while.js
+++ js/do_while.js
@@ -1,2 +1,8 @@
 "use strict";
 
+//  TODO: An ice cream seller can't go home until she sells all of her cones. First write enough code that generates a random number between 50 and 100 representing the amount of cones to sell before you start your loop. Inside of the loop your code should generate another random number between 1 and 5, simulating the amount of cones being bought by her clients. Use a do-while loop to log to the console the amount of cones sold to each person. This is a way get the random numbers for this exercise.
+var numCones = Math.floor(Math.random()*50)+50;
+do{
+    var amtConesBought = Math.floor(Math.random()*5)+1;
+    console.log(`Thank you for purchasing ${amtConesBought} cones today!`);
+}while( < numCones)
