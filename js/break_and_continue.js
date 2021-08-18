99a17649c573c8e34f33f998a8329aa06898ed8f
Julian Martinez <julianmartinez1830@gmail.com>
2021-08-17 19:20:51 -0500

    feat: cleaned up code and added TODOs for clearer instruction

--- js/break_and_continue.js
+++ js/break_and_continue.js
@@ -1,6 +1,7 @@
 "use strict";
 
-
+//  TODO: Prompt the user for an odd number between 1 and 50. Use a loop and a break statement to continue prompting the user if they enter invalid
+//  input.
 // for(var i=1; i <= 50; i++){
 //     var userInput = prompt("Select an odd number between 1 and 50");
 //     var numPick = Number(userInput);
@@ -13,7 +14,7 @@
 // }
 // console.log("You've reached the end of the loop or you BROKE out!");
 
-// Use a loop and the continue statement to output all the odd numbers between 1 and 50, except for the number the user entered.
+//  TODO: Use a loop and the continue statement to output all the odd numbers between 1 and 50, except for the number the user entered.
 var userInput = prompt("Select an odd number between 1 and 50");
 var numPick = Number(userInput);
 console.log(`Number to skip ${numPick}`);
@@ -26,8 +27,4 @@ for(var i=1; i<50; i++){
         }
         console.log(`${i} is an odd number!`);
     }
-    // if(numPick === i){
-    //     console.log(`Yikes, we are skipping ${numPick}`);
-    //     continue;
-    // }
 }
