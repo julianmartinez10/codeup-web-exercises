01fa689b005833087343ab882d3c723a434c3138
Julian Martinez <julianmartinez1830@gmail.com>
2021-08-19 18:20:49 -0500

    feat: tasks 4-7 completed

--- js/planets-array.js
+++ js/planets-array.js
@@ -30,14 +30,19 @@
     planets.shift()
     console.log(planets);
 
-    // console.log('Removing "Pluto" from the end of the planets array.');
-    // console.log(planets);
-    //
-    // console.log('Finding and logging the index of "Earth" in the planets array.');
-    //
-    // console.log("Reversing the order of the planets array.");
-    // console.log(planets);
-    //
-    // console.log("Sorting the planets array.");
-    // console.log(planets);
+    console.log('Removing "Pluto" from the end of the planets array.');
+    planets.pop();
+    console.log(planets);
+
+    console.log('Finding and logging the index of "Earth" in the planets array.');
+    var indexPlanet = planets.indexOf('Earth');
+    console.log(indexPlanet);
+
+    console.log("Reversing the order of the planets array.");
+    planets.reverse()
+    console.log(planets);
+
+    console.log("Sorting the planets array.");
+    planets.sort();
+    console.log(planets);
 })();
